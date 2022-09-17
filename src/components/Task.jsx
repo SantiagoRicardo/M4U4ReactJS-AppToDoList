513c9055561bb5fc0925761c1cad218639eadc6f
Santiago Ricardo Ramirez <sricard2@ibero.edu.co>
2022-09-17 02:07:38 -0500

    Botones De Actualizar y Editar Tareas Han Sido Creados

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,11 @@
-const Task = ({tasks}) => {
-
-  const {title, date, description} = tasks
+const Task = ({ tasks }) => {
+  const { title, date, description } = tasks;
 
   return (
     <div className="bg-white shadow-md px-5 py-10 rounded-lg mt-5">
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Title: <span className="font-normal normal-case">{title}</span>
-      </p> 
+      </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Date: <span className="font-normal normal-case">{date}</span>
       </p>
@@ -14,6 +13,21 @@ const Task = ({tasks}) => {
         Description:{" "}
         <span className="font-normal normal-case">{description}</span>
       </p>
+
+      <div className="flex justify-between">
+        <button
+          className="bg-green-600 hover:bg-green-700 mt-4 py-2 px-10 rounded-md text-white font-bold"
+          type="buttom"
+        >
+          Update
+        </button>
+        <button
+          className="bg-red-600 hover:bg-red-700 mt-4 py-2 px-10 rounded-md text-white font-bold"
+          type="buttom"
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,21 +1,27 @@
 import Task from "./Task";
 
-export const TaskList = ({task}) => {
+export const TaskList = ({ task }) => {
   console.log(task);
   return (
     <div className="md:w-1/2 lg:w-2/5 mx-5 mb-10 md:h-screen overflow-scroll">
-      <h2 className="font-black text-3xl text-center mb-10">Task List</h2>
+      {task && task.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center mb-10">
+            Task List
+          </h2>
 
-      {task.map((tasks) => {
-        return(
-          <Task
-            key={tasks.id}
-            tasks={tasks}
-          />
-        )
-      })}
+          {task.map((tasks) => {
+            return <Task key={tasks.id} tasks={tasks} />;
+          })}
+        </>
+      ) : (
+        <h2 className="font-black text-3xl text-center mb-10">
+          No Tasks
+        </h2>
+        
+      )}
     </div>
-  );  
+  );
 };
 
 export default TaskList;
